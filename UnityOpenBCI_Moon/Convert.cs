e146408f51b271395cd35a3414c3c0d7729236e1
jung moon <moon@sogang.ac.kr>
2019-09-23 08:25:27 +0900

    InitUnityOpenBCI

---
+++ UnityOpenBCI_Moon/Convert.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBCI_GUI
+{
+    class Convert
+    {
+        public static int Bit16ToInt32(byte[] byteArray)
+        {
+            int result = (
+              ((0xFF & byteArray[0]) << 8) |
+               (0xFF & byteArray[1])
+              );
+            if ((result & 0x00008000) > 0)
+            {
+                result = (int)((uint)result | (uint)0xFFFF0000);
+
+            }
+            else
+            {
+                result = (int)((uint)result & (uint)0x0000FFFF);
+            }
+            return result;
+        }
+        public static int Bit24ToInt32(byte[] byteArray)
+        {
+            int result = (
+                 ((0xFF & byteArray[0]) << 16) |
+                 ((0xFF & byteArray[1]) << 8) |
+                 (0xFF & byteArray[2])
+               );
+            if ((result & 0x00800000) > 0)
+            {
+                result = (int)((uint)result | (uint)0xFF000000);
+            }
+            else
+            {
+                result = (int)((uint)result & (uint)0x00FFFFFF);
+            }
+            return result;
+        }
+
+        static double[] ConvertedData = new double[12];
+        private static int localByteCounter = 0;
+        private static int localChannelCounter = 0;
+        private static int PACKET_readstate = 0;
+        private static byte[] localAdsByteBuffer = { 0, 0, 0 };
+        private static byte[] localAccelByteBuffer = { 0, 0 };
+
+//        Header
+
+//Byte 1: 0xA0
+//Byte 2: Sample Number
+//EEG Data
+//Note: values are 24-bit signed, MSB first
+
+//Bytes 3-5: Data value for EEG channel 1; 3 bytes for each channel
+//Bytes 6-8: Data value for EEG channel 2
+//Bytes 9-11: Data value for EEG channel 3
+//Bytes 12-14: Data value for EEG channel 4
+//Bytes 15-17: Data value for EEG channel 5
+//Bytes 18-20: Data value for EEG channel 6
+//Bytes 21-23: Data value for EEG channel 6
+//Bytes 24-26: Data value for EEG channel 8
+//Aux Data
+
+//Bytes 27-32: 6 bytes of data defined and parsed based on the Footer below
+//Footer
+
+//Byte 33: 0xCX where X is 0-F in hex
+
+        public static double[] interpretBinaryStream(byte actbyte)
+        {
+            bool flag_copyRawDataToFullData = false;
+
+            switch (PACKET_readstate) // the state transition for the finite automata
+            {
+                case 0:
+                    if (actbyte == 0xC0)//Stop Byte = Byte 33: 0xCX where X is 0-F in hex
+                                        //                        The following table is sorted by Stop Byte. 
+                                        //Drivers should use the Stop Byte to determine how to parse the 6 AUX bytes.
+
+                        //Stop Byte   Byte 27 Byte 28 Byte 29 Byte 30 Byte 31 Byte 32
+                        //0xC0    AX1 AX0 AY1 AY0 AZ1 AZ0
+                        //AX1 - AX0: Data value for accelerometer channel X 
+                        //AY1 - AY0: Data value for accelerometer channel Y 
+                        //AZ1 - AZ0: Data value for accelerometer channel Z
+
+                     {          // poszukiwanie poczatku pakietu
+                        PACKET_readstate++; // Stop byte =>  PACKET_readstate becomes 1 for the next byte in the stream
+                    }
+                    break;
+                case 1:
+                    if (actbyte == 0xA0) // Packet Header =         Byte 1: 0xA0
+                    {          // poszukiwanie poczatku pakietu
+                        PACKET_readstate++; //  Header Byte =>  PACKET_readstate becomes 2 for the next byte in the stream
+                    }
+                    else
+                    {
+                        PACKET_readstate = 0;
+                    }
+                    break;
+                case 2: // Reading the sample number
+                    localByteCounter = 0;
+                    localChannelCounter = 0;
+                    ConvertedData[localChannelCounter] = actbyte; // localChannelCounter ==0: the sample number
+
+                    localChannelCounter++; // channel counter becomes 1 for the 1st channel
+
+                    PACKET_readstate++; // PACKET_readstate becomes 3 for reading data bytes
+
+                    break;
+                case 3: // the state in which to read channel data bytes
+                    localAdsByteBuffer[localByteCounter] = actbyte;
+
+                    localByteCounter++;
+                    if (localByteCounter == 3) // 3 bytes are read for the current channel
+                    {
+                        ConvertedData[localChannelCounter] = Bit24ToInt32( localAdsByteBuffer) ;
+
+                        localChannelCounter++; // / Rhe channel counter increases for the next channel
+                        if (localChannelCounter == 9) // all the 8 channel data are read
+                        {
+                            PACKET_readstate++; // PACKET_readstate becomes 4: All of the 8 channel data are read.
+
+                            localByteCounter = 0;
+                        }
+                        else
+                        {
+                            localByteCounter = 0; // PACKET_readstate remains to be 3: continue to read channel data bytes.
+                        }
+                    }
+                    break;
+                case 4: // All of the 8 channel data are read. read the auxiliary data of 6 bytes
+                    localAccelByteBuffer[localByteCounter] = actbyte; //  localByteCounter = 0
+
+                    localByteCounter++;
+                    if (localByteCounter == 2)
+                    {
+                        ConvertedData[ localChannelCounter ] = Bit16ToInt32( localAccelByteBuffer );
+
+                        localChannelCounter++;
+
+                        if (localChannelCounter == 12) // have read the two auxiliary 3 byte items 
+                        {
+                            PACKET_readstate++; // PACKET_readstate becomes 5
+                            localByteCounter = 0;
+                        }
+                        else
+                        {
+                            localByteCounter = 0; // PACKET_readstate remains to be the same (4)
+                        }
+                    }
+                    break;
+                case 5: // All of the 8 channel data plus the auxiliary 2 * 3 bytes are read
+                    if (actbyte == 0xC0) // //Stop Byte   Byte 27 Byte 28 Byte 29 Byte 30 Byte 31 Byte 32
+                                         //0xC0    AX1 AX0 AY1 AY0 AZ1 AZ0
+                                         //AX1 - AX0: Data value for accelerometer channel X 
+                                         //AY1 - AY0: Data value for accelerometer channel Y 
+                                         //AZ1 - AZ0: Data value for accelerometer channel Z
+                    {
+                        flag_copyRawDataToFullData = true;  // the current occurrence of the 8 channel data is completed
+                        PACKET_readstate = 1;
+                    }
+                    else
+                    {
+                        PACKET_readstate = 0;
+                    }
+
+                    break;
+                default:
+                    PACKET_readstate = 0;
+                    break;
+            }//    switch (PACKET_readstate)
+
+            if (flag_copyRawDataToFullData)
+            {
+                flag_copyRawDataToFullData = false;
+                return ConvertedData; //// the current occurrence of the 8 channel data is completed => return the converted data
+            }
+            else
+            {
+                return null; ///// the current occurrence of the 8 channel data is NOT completed  ==> return null 
+            }
+        } //  public static double[] interpretBinaryStream(byte actbyte)
+    }//    class Convert
+}//namespace OpenBCI_GUI
---
+++ UnityOpenBCI_Moon/Form1.cs
@@ -0,0 +1,520 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace OpenBCI_GUI
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter sw;
+        bool istniejeSW = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+
+            //The Cyton board has an on-board RFDuino radio module acting as a "Device". 
+            //The Cyton system includes a USB dongle for the PC, which acts as the RFDuino "Host".
+
+            // USB has the rate of data transfer 12 Mbps for disk-drives and 1.5Mbps for devices
+            // that need less bandwidth.
+            // Serial ports are also known as communication (COM) ports = 11200bps.
+            //https://github.com/OpenBCI/Docs/blob/master/Hardware/03-Cyton_Data_Format.md
+            //Each FTDI adapter contains a USB microcontroller which talks a proprietary protocol 
+            //via USB and transforms that into the regular UART signals and vice versa.
+
+            //The RFDuino USB dongle (the RFDuino "Host") is connected to an FTDI (USB<->Serial)
+            // converter configured to appear to the computer as if it is a standard serial port running 
+            //at a rate of 115200 baud using the typical 8-N-1.
+
+            comboBox1.SelectedItem = "COM16";
+
+            textBox2.Text = Application.StartupPath;
+            textBox3.Text = "test.txt";
+
+            radioButton4.Checked = true;
+            radioButton5.Checked = true;
+            radioButton8.Checked = true;
+
+            button1.Enabled = true;
+
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+
+            checkBox1.Checked = true;
+            checkBox2.Checked = true;
+            checkBox3.Checked = true;
+            checkBox4.Checked = true;
+            checkBox5.Checked = true;
+            checkBox6.Checked = true;
+            checkBox7.Checked = true;
+            checkBox8.Checked = true;
+        }
+        
+        struct DaneSerialPort // bytes array at arrived at the serial port
+        {
+            public byte[] zmienna; // buffer
+        };
+
+        Queue<DaneSerialPort> driver = new Queue<DaneSerialPort>(); // driver = queue of the arrived byte arrays.
+
+        // Event Handler
+        void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            DaneSerialPort odebrane_dane; //the arrived byte array.
+
+            byte[] buffer = new byte[serialPort1.BytesToRead];
+
+            serialPort1.Read(buffer, 0, buffer.Length);
+
+            odebrane_dane.zmienna = buffer;
+
+            driver.Enqueue(odebrane_dane); // add the current buffer content to the buffer queue
+        }
+
+        // Event Handler
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text;
+            serialPort1.BaudRate = 115200;
+            try
+            {
+                serialPort1.Open();
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Nie można otworzyć portu" + comboBox1.Text.ToString());
+            }
+            serialPort1.DataReceived += serialPort1_DataReceived;
+            // delegate void SerialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e) DataReceived ; => this creates a new delegate (pointer)
+        }
+
+        // Event Handler
+        private void button2_Click(object sender, EventArgs e)
+        {
+            turnOFF_SW();
+            turnOFF_transmision();
+            serialPort1.Close();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+        }
+
+        //Once the OpenBCI has initialized itself and sent the $$$, it waits for commands.
+        //    In other words, it sends no data until it is told to start sending data.To begin data transfer, transmit a single ASCII b.
+        //    Once the b is received, continuous transfer of data in binary format will ensue. To turn off the fire hose, send an s.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            char[] buff = new char[1];
+            buff[0] = 'b';  //  Once the b is received, continuous transfer of data in binary format will ensue
+            try
+            {
+                serialPort1.Write(buff, 0, 1);// Write(char[] buffer, int offset, int count);
+            }
+            catch
+            {
+                MessageBox.Show("Nie można uruchomic.");
+            }
+            button3.Enabled = false;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            turnOFF_SW();
+            turnOFF_transmision();////To turn off the fire hose, send an s.
+            button4.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Directory.CreateDirectory(textBox2.Text);
+                sw = new StreamWriter(Path.Combine(textBox2.Text, textBox3.Text));
+                istniejeSW = true;
+            }
+            catch
+            {
+                MessageBox.Show("Nie można utworzyć katalogu bezpośrednio na głównej partycji.");
+            }
+            button5.Enabled = false;
+            button6.Enabled = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            turnOFF_SW();
+            button5.Enabled = true;
+            button6.Enabled = false;
+        }
+
+        // Event Handler
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double[] daneRys;
+
+            // read all the data in the queue:
+
+            while (driver.Count > 0) // driver = the queue of buffer contents
+            {
+                // get the current data buffer at the front of the queue
+                DaneSerialPort data = driver.Dequeue(); // driver.Count decreases
+                for (int g = 0; g < data.zmienna.Length; g++)
+                {   // interpret the current byte data.zmienna[g]
+
+                    daneRys = Convert.interpretBinaryStream( data.zmienna[g] ); // public byte[] zmienna; // buffer
+
+                    if (daneRys != null) // the 8 channel data is completely read
+                    {
+                        daneRys = ValueOrZero(daneRys); // daneRys = array of 8 places => ValueOrZero sets the non-activated channel to 0
+                                                        //  which indicates which channel is active.
+                        writeToFile(daneRys);
+                        drawPlot(daneRys); 
+                    }
+
+                   // else: get the next byte
+                } // get the next buffer at the queue
+            }
+
+        }
+
+        private void writeToFile(double[] daneRys)
+        {
+            //ScaleFactor (Volts/count) = 4.5 Volts / gain / (2^23 - 1); gain =24; 
+
+            double mnoz = (4.5 / 24 / (Math.Pow(2, 23) - 1)) * (Math.Pow(10, 6));
+
+            for (int i = 0; i < 8; i++)
+            {
+                daneRys[i + 1] = daneRys[i + 1] * mnoz;
+            }
+            if (istniejeSW)
+            {
+                sw.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11}", daneRys[0], daneRys[1], daneRys[2], daneRys[3], daneRys[4], daneRys[5], daneRys[6], daneRys[7], daneRys[8], daneRys[9], daneRys[10], daneRys[11]);
+            }
+        }
+
+        private void drawPlot(double[] dane)
+        {
+            dane = filtering(dane);
+            //DataPoint Add(params double[] y);
+            // public int AddXY(double xValue, double yValue);
+
+            chart1.Series[0].Points.Add(dane[0]); // X= 1, Y= dane[0] = the sample number => chart1; chart1.Series[i] is the ith graph; dane[0] is the first element of params array y
+                chart2.Series[0].Points.Add(dane[1]);  // X=2; Y= dane[1] = the 3 byte data for the channel 1 => chart2. 
+            chart3.Series[0].Points.Add(dane[2]);  // X=3, Y= dane[2] = the 3 byte data for the channel 2 => chart3
+                chart4.Series[0].Points.Add(dane[3]);//...
+                chart5.Series[0].Points.Add(dane[4]);
+                chart6.Series[0].Points.Add(dane[5]);
+                chart7.Series[0].Points.Add(dane[6]);
+                chart8.Series[0].Points.Add(dane[7]);
+                chart9.Series[0].Points.Add(dane[8]);   // X=9, Y = dane[9] = the 3 byte data for the channel 9 => chart10
+
+            while (chart1.Series[0].Points.Count > 1250)  // chart1 has more than 1250 points ; Points are added as a group of 8 points each time when this method
+                                                          // is executed => remove the data until it is less than 1250
+                {
+                    chart1.Series[0].Points.SuspendUpdates();
+                    chart1.Series[0].Points.Remove(chart1.Series[0].Points.First());
+                    chart1.Series[0].Points.ResumeUpdates();
+
+                    chart2.Series[0].Points.SuspendUpdates();
+                    chart2.Series[0].Points.Remove(chart2.Series[0].Points.First());
+                    chart2.Series[0].Points.ResumeUpdates();
+
+                    chart3.Series[0].Points.SuspendUpdates();
+                    chart3.Series[0].Points.Remove(chart3.Series[0].Points.First());
+                    chart3.Series[0].Points.ResumeUpdates();
+
+                    chart4.Series[0].Points.SuspendUpdates();
+                    chart4.Series[0].Points.Remove(chart4.Series[0].Points.First());
+                    chart4.Series[0].Points.ResumeUpdates();
+
+                    chart5.Series[0].Points.SuspendUpdates();
+                    chart5.Series[0].Points.Remove(chart5.Series[0].Points.First());
+                    chart5.Series[0].Points.ResumeUpdates();
+
+                    chart6.Series[0].Points.SuspendUpdates();
+                    chart6.Series[0].Points.Remove(chart6.Series[0].Points.First());
+                    chart6.Series[0].Points.ResumeUpdates();
+
+                    chart7.Series[0].Points.SuspendUpdates();
+                    chart7.Series[0].Points.Remove(chart7.Series[0].Points.First());
+                    chart7.Series[0].Points.ResumeUpdates();
+
+                    chart8.Series[0].Points.SuspendUpdates();
+                    chart8.Series[0].Points.Remove(chart8.Series[0].Points.First());
+                    chart8.Series[0].Points.ResumeUpdates();
+
+                    chart9.Series[0].Points.SuspendUpdates();
+                    chart9.Series[0].Points.Remove(chart9.Series[0].Points.First());
+                    chart9.Series[0].Points.ResumeUpdates();
+            }// while (chart1.Series[0].Points.Count > 1250)  // chart1 has more than 1250 data 
+        }//private void drawPlot(double[] dane)
+
+        private double[] ValueOrZero(double[] dane)
+        {
+            if (!checkBox1.Checked) // channel 1 is not set for reading
+            {
+                dane[1] = 0;  // channel 1 is not set for reading => set the read data to zero
+            }
+            if (!checkBox2.Checked) // channel 2 is not set for reading
+            {
+                dane[2] = 0; // channel 2 is not set for reading => set the read data to zero
+            }
+            if (!checkBox3.Checked)
+            {
+                dane[3] = 0;// channel 3 is not set for reading => set the read data to zero
+            }
+            if (!checkBox4.Checked)
+            {
+                dane[4] = 0; // channel 4 is not set for reading => set the read data to zero
+            }
+            if (!checkBox5.Checked)
+            {
+                dane[5] = 0;
+            }
+            if (!checkBox6.Checked)
+            {
+                dane[6] = 0;
+            }
+            if (!checkBox7.Checked)
+            {
+                dane[7] = 0;
+            }
+            if (!checkBox8.Checked)
+            {
+                dane[8] = 0;
+            }
+            return dane;
+        }
+
+        //https://neurobb.com/t/openbci-why-are-1-50hz-bandpass-and-60hz-notch-filters-both-applied-by-default/23/2
+
+        private double[] filtering(double[] dane)
+        {
+            int standard = 0;
+            int notch = 0;
+
+            if (radioButton5.Checked)
+            {
+                notch = 0;
+            }
+            else if (radioButton6.Checked)
+            {
+                notch = 1;
+            }
+            else if (radioButton7.Checked)
+            {
+                notch = 2;
+            }
+            if (radioButton8.Checked)
+            {
+                standard = 0;
+            }
+            if (radioButton9.Checked)
+            {
+                standard = 1;
+            }
+            if (radioButton10.Checked)
+            {
+                standard = 2;
+            }
+            if (radioButton11.Checked)
+            {
+                standard = 3;
+            }
+            if (radioButton12.Checked)
+            {
+                standard = 4;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                dane[i + 1] = Filters.FiltersSelect(standard, notch, dane[i + 1], i);
+            }
+
+            return dane;
+        }
+
+        private void turnOFF_SW()
+        {
+            if (istniejeSW)
+            {
+                istniejeSW = false;
+                sw.Close();
+            }
+        }
+        private void turnOFF_transmision()
+        {
+            char[] buff = new char[1];
+            buff[0] = 's';//To turn off the fire hose, send an s.
+            serialPort1.Write(buff, 0, 1);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //-1V 1V
+            chart2.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart2.ChartAreas[0].AxisY.Minimum = -1000000;
+
+            chart3.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart3.ChartAreas[0].AxisY.Minimum = -1000000;
+
+            chart4.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart4.ChartAreas[0].AxisY.Minimum = -1000000;
+
+            chart5.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart5.ChartAreas[0].AxisY.Minimum = -1000000;
+
+            chart6.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart6.ChartAreas[0].AxisY.Minimum = -1000000;
+
+            chart7.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart7.ChartAreas[0].AxisY.Minimum = -1000000;
+
+            chart8.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart8.ChartAreas[0].AxisY.Minimum = -1000000;
+
+            chart9.ChartAreas[0].AxisY.Maximum = 1000000;
+            chart9.ChartAreas[0].AxisY.Minimum = -1000000;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //-100mV 100mV
+            chart2.ChartAreas[0].AxisY.Maximum = 100000;
+            chart2.ChartAreas[0].AxisY.Minimum = -100000;
+
+            chart3.ChartAreas[0].AxisY.Maximum = 100000;
+            chart3.ChartAreas[0].AxisY.Minimum = -100000;
+
+            chart4.ChartAreas[0].AxisY.Maximum = 100000;
+            chart4.ChartAreas[0].AxisY.Minimum = -100000;
+
+            chart5.ChartAreas[0].AxisY.Maximum = 100000;
+            chart5.ChartAreas[0].AxisY.Minimum = -100000;
+
+            chart6.ChartAreas[0].AxisY.Maximum = 100000;
+            chart6.ChartAreas[0].AxisY.Minimum = -100000;
+
+            chart7.ChartAreas[0].AxisY.Maximum = 100000;
+            chart7.ChartAreas[0].AxisY.Minimum = -100000;
+
+            chart8.ChartAreas[0].AxisY.Maximum = 100000;
+            chart8.ChartAreas[0].AxisY.Minimum = -100000;
+
+            chart9.ChartAreas[0].AxisY.Maximum = 100000;
+            chart9.ChartAreas[0].AxisY.Minimum = -100000;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            //-10mv 10mv
+            chart2.ChartAreas[0].AxisY.Maximum = 10000;
+            chart2.ChartAreas[0].AxisY.Minimum = -10000;
+
+            chart3.ChartAreas[0].AxisY.Maximum = 10000;
+            chart3.ChartAreas[0].AxisY.Minimum = -10000;
+
+            chart4.ChartAreas[0].AxisY.Maximum = 10000;
+            chart4.ChartAreas[0].AxisY.Minimum = -10000;
+
+            chart5.ChartAreas[0].AxisY.Maximum = 10000;
+            chart5.ChartAreas[0].AxisY.Minimum = -10000;
+
+            chart6.ChartAreas[0].AxisY.Maximum = 10000;
+            chart6.ChartAreas[0].AxisY.Minimum = -10000;
+
+            chart7.ChartAreas[0].AxisY.Maximum = 10000;
+            chart7.ChartAreas[0].AxisY.Minimum = -10000;
+
+            chart8.ChartAreas[0].AxisY.Maximum = 10000;
+            chart8.ChartAreas[0].AxisY.Minimum = -10000;
+
+            chart9.ChartAreas[0].AxisY.Maximum = 10000;
+            chart9.ChartAreas[0].AxisY.Minimum = -10000;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            //-1mV
+            chart2.ChartAreas[0].AxisY.Maximum = 1000;
+            chart2.ChartAreas[0].AxisY.Minimum = -1000;
+
+            chart3.ChartAreas[0].AxisY.Maximum = 1000;
+            chart3.ChartAreas[0].AxisY.Minimum = -1000;
+
+            chart4.ChartAreas[0].AxisY.Maximum = 1000;
+            chart4.ChartAreas[0].AxisY.Minimum = -1000;
+
+            chart5.ChartAreas[0].AxisY.Maximum = 1000;
+            chart5.ChartAreas[0].AxisY.Minimum = -1000;
+
+            chart6.ChartAreas[0].AxisY.Maximum = 1000;
+            chart6.ChartAreas[0].AxisY.Minimum = -1000;
+
+            chart7.ChartAreas[0].AxisY.Maximum = 1000;
+            chart7.ChartAreas[0].AxisY.Minimum = -1000;
+
+            chart8.ChartAreas[0].AxisY.Maximum = 1000;
+            chart8.ChartAreas[0].AxisY.Minimum = -1000;
+
+            chart9.ChartAreas[0].AxisY.Maximum = 1000;
+            chart9.ChartAreas[0].AxisY.Minimum = -1000;
+        }
+
+        private void radioButton13_CheckedChanged(object sender, EventArgs e)
+        {
+            //-100uV
+            chart2.ChartAreas[0].AxisY.Maximum = 100;
+            chart2.ChartAreas[0].AxisY.Minimum = -100;
+
+            chart3.ChartAreas[0].AxisY.Maximum = 100;
+            chart3.ChartAreas[0].AxisY.Minimum = -100;
+
+            chart4.ChartAreas[0].AxisY.Maximum = 100;
+            chart4.ChartAreas[0].AxisY.Minimum = -100;
+
+            chart5.ChartAreas[0].AxisY.Maximum = 100;
+            chart5.ChartAreas[0].AxisY.Minimum = -100;
+
+            chart6.ChartAreas[0].AxisY.Maximum = 100;
+            chart6.ChartAreas[0].AxisY.Minimum = -100;
+
+            chart7.ChartAreas[0].AxisY.Maximum = 100;
+            chart7.ChartAreas[0].AxisY.Minimum = -100;
+
+            chart8.ChartAreas[0].AxisY.Maximum = 100;
+            chart8.ChartAreas[0].AxisY.Minimum = -100;
+
+            chart9.ChartAreas[0].AxisY.Maximum = 100;
+            chart9.ChartAreas[0].AxisY.Minimum = -100;
+        }
+        
+    }
+}
